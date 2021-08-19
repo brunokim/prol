7890cf78286684242d1d75e13cd747ab825ab099
Bruno Kim <brunokim.mc@gmail.com>
2021-08-19 20:29:36 -0300

    Introducing unit tests for interpreter

---
+++ test_interpreter.py
@@ -0,0 +1,57 @@
+import pytest
+from interpreter import *
+
+package = [
+    # member(E, [H|T]) :- member_(T, E, H).
+    # member_(_, E, E).
+    # member_([H|T], E, _) :- member_(T, E, H).
+    Clause(Struct("member", Var("E"), Struct(".", Var("H"), Var("T"))),
+           Struct("member_", Var("T"), Var("E"), Var("H"))),
+    Clause(Struct("member_", Var("_"), Var("E"), Var("E"))),
+    Clause(Struct("member_", Struct(".", Var("H"), Var("T")), Var("E"), Var("_")),
+           Struct("member_", Var("T"), Var("E"), Var("H"))),
+
+    # length([], 0).
+    # length([_|T], s(L)) :- length(T, L).
+    Clause(Struct("length", Atom("[]"), Atom("0"))),
+    Clause(Struct("length", Struct(".", Var("_"), Var("T")), Struct("s", Var("L"))),
+           Struct("length", Var("T"), Var("L"))),
+]
+
+testdata = [
+    ([
+        # ?- length(L, s(s(s(0)))), member(a, L).
+        Struct("length", Var("L"), Struct(
+            "s", Struct("s", Struct("s", Atom("0"))))),
+        Struct("member", Atom("a"), Var("L")),
+    ], [
+        Solution({Var("L"): Struct(".", Atom("a"), Struct(
+            ".", Var("_"), Struct(".", Var("_"), Atom("[]"))))}),
+        Solution({Var("L"): Struct(".", Var("_"), Struct(
+            ".", Atom("a"), Struct(".", Var("_"), Atom("[]"))))}),
+        Solution({Var("L"): Struct(".", Var("_"), Struct(
+            ".", Var("_"), Struct(".", Atom("a"), Atom("[]"))))}),
+    ]),
+]
+
+
+def ignore_vars(term: Term) -> Term:
+    if isinstance(term, Atom):
+        return term
+    if isinstance(term, Var):
+        return Var("_")
+    if isinstance(term, Struct):
+        return Struct(term.name, *(ignore_vars(arg) for arg in term.args))
+    raise ValueError(f"unexpected term type {type(term)}")
+
+
+def ignore_vars_in_solution(solution: Solution) -> Solution:
+    return Solution({x: ignore_vars(term) for x, term in solution.items()})
+
+
+@pytest.mark.parametrize("query, solutions", testdata)
+def test_interpreter(query, solutions):
+    wam = Machine(package, query)
+    for got, want in zip(wam.run(), solutions):
+        got = ignore_vars_in_solution(got)
+        assert got == want
