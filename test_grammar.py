7e2c3d48944aa67fd0b3d137e4ad6106f80c7d52
Bruno Kim <brunokim.mc@gmail.com>
2021-08-30 02:11:40 -0300

    Test parsing quoted atoms, and terms with spaces

--- test_grammar.py
+++ test_grammar.py
@@ -5,11 +5,15 @@ import pytest
 
 @pytest.mark.parametrize('text, term', [
     ('a', Atom('a')),
+    ('  a', Atom('a')),
+    (' a  ', Atom('a')),
+    ('a\n  ', Atom('a')),
     ('b', Atom('b')),
     ('z', Atom('z')),
     ('a123', Atom('a123')),
     (':', Atom(':')),
     (':-', Atom(':-')),
+    ("'('", Atom('(')),
 ])
 def test_parse_term(text, term):
     assert parse_term(text) == term
